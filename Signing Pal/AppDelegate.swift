0354bfa2ed65b2a13826f42bccf360a695a51eb1
Andrew <Pirkl>
2020-06-03 19:22:12 -0500

    Initial Commit

---
+++ Signing Pal/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Signing Pal
+//
+//  Created by Andrew Pirkl on 6/3/20.
+//  Copyright © 2020 Pirklator. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
